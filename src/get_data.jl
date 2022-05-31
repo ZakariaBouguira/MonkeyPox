8cf396f05a2671a225f1626d7b9dad5b361c8a4e
Zakaria Bouguira <zakaria.bouguira@gmail.com>
2022-05-31 19:00:32 +0100

    This project aims to provide a time series updated every hour about the 2022 Monkey Pox outbreak.
    # MonkeyPox
    
    This project aims to provide a time series updated every hour about the 2022 Monkey Pox outbreak.
    
    For now only global time series is available.
    For now only confirmed cases are selected to build the time series monkeypox_time_serie.csv.
    All data come from Global.health.
    monkeypox_data.csv is a csv file representing raw data of Global.health
    Some graphs and some animations are generated from the time series
    
    Feel free to use it in any way you want but please, please push for a world without this virus and without any of the other deadly diseases.
    "Living with the virus" is not a fatality it's a choice made for us.

---
+++ src/get_data.jl
@@ -0,0 +1,46 @@
+using DataFrames, CSV
+using Statistics: mean
+using Queryverse
+using Dates
+
+
+url = "https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv"
+download(url, "data/monkeypox_data.csv")
+df = DataFrame(CSV.File("data/monkeypox_data.csv"))
+
+df = df |>
+    @filter(_.Status=="confirmed")|>
+    @orderby(_.Date_confirmation) |>
+    @groupby(_.Date_confirmation) |>
+    @map({Date_confirmation=key(_), Count_infected=length(_)}) |>
+    @filter(!isna(_.Date_confirmation)) |>
+    DataFrame 
+
+firstDate = df.Date_confirmation[1]
+lastDate = df.Date_confirmation[end]
+dr = collect(firstDate:Dates.Day(1):lastDate)
+dic = Dict(Pair.(df.Date_confirmation, df.Count_infected))
+
+infectedNew = []
+for i in 1:length(dr)
+    if in.(dr[i], [Set(df.Date_confirmation)]) == Bool[1]
+        push!(infectedNew, dic[dr[i]])
+    else
+        push!(infectedNew, 0)
+    end
+    
+end
+
+infectedTotal = [0]
+for i in 1:length(infectedNew)
+    push!(infectedTotal, infectedTotal[i]+infectedNew[i])
+end
+infectedTotal = infectedTotal[2:end]
+
+dataSet = DataFrame(date = dr,
+                    infectedNew = infectedNew,
+                    infectedTotal = infectedTotal
+                    )
+
+CSV.write("data/monkeypox_time_serie.csv", dataSet)
+movingaverage(g, n) = [i < n ? mean(g[begin+n÷2:i+n÷2]) : mean(g[i+n÷2-n+1:i]) for i in 1+n÷2:length(g)];
